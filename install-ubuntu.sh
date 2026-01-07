c5e67725866b65dcf961a7fdcda993d5994ab215
amirmnoohi <amirmnoohi@gmail.com>
2026-01-07 02:50:30 +0000

    Fix Ubuntu install: use venv for PEP 668 compliance

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -1,29 +1,39 @@
 #!/usr/bin/env bash
 
 INSTALL_DIR=/opt/ocservapi
+VENV_DIR=$INSTALL_DIR/venv
 
 ################# INSTALL PRE REQUIREMENTS #################
 apt update
-apt install python3 python3-pip python3-dev build-essential -y
+apt install python3 python3-pip python3-venv python3-dev build-essential -y
 
-pip3 install flask
-
-pip3 install uwsgi
 ############################################################
 
 ################## Install OCSERV-API ######################
 
 mkdir -p $INSTALL_DIR
 
-cd .. && mv ocserv-api/* $INSTALL_DIR && cd $INSTALL_DIR || exit
+cp -r . $INSTALL_DIR
+cd $INSTALL_DIR || exit
 
 chmod +x update.sh
 
 ############################################################
 
+################# CREATE VIRTUAL ENVIRONMENT ###############
+
+python3 -m venv $VENV_DIR
+$VENV_DIR/bin/pip install --upgrade pip
+$VENV_DIR/bin/pip install flask uwsgi
+
+############################################################
+
 ############## Install OCSERV-API SERVICE ##################
 
-mv ocserv-api.service /etc/systemd/system
+cp ocserv-api.service /etc/systemd/system/ocserv-api.service
+
+# Update service file to use venv uwsgi
+sed -i "s|/usr/local/bin/uwsgi|$VENV_DIR/bin/uwsgi|g" /etc/systemd/system/ocserv-api.service
 
 systemctl daemon-reload
 ############################################################
